ab3c265188f23e94ecea4dd53f872ed7533e9a6a
Aaron J. Maxwell, PhD - I only answer queries on the fifth Wednesday of each month <ajmax.85@gmail.com>
2021-10-16 00:22:07 -0400

    Finished the password generator.

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ My copy of the KLC Code Words with Python project.
 
 ## Goals of the project
 The learning objectives are given in the Instructor manual.
+
+### Crack Passwords
+```
+from CrackDemPWCLC.getCracking import attack
+for p in ["Refrigerating", "13579", "H4CK3R", "W3lc0m3!"]:
+    attack(p)
+```
---
+++ genSomeFavWords.py
@@ -0,0 +1,33 @@
+"""Set up the Cracking Codes.
+
+Step 1: Get the students to build a new project.
+Step 2: Have them print something!
+Step 3: Have them generate lists of favourite words and characters.
+Step 4: Have them pick a new random word, character, number. Import random!
+Step 5: Have them join the three together and print!
+Bonus: Have them
+
+@author: Bilal Qadar
+@organization: Canada Learning Code
+"""
+__author__ = "AJMax"
+import random
+
+print("Your new password is:")
+
+favourite_words = ["orange", "cat", "jamaica", "ladder", "CLC"]
+favourite_characters = ["^", ">", "<", "~"]
+
+chosen_word = random.choice(favourite_words)
+chosen_char = random.choice(favourite_characters)
+
+chosen_num = random.randint(1, 100)
+
+new_password = chosen_word + str(chosen_num) + chosen_char
+print(new_password)
+
+# Bonus
+# new_password = [chosen_word, str(chosen_num), chosen_char]
+# random.shuffle(new_password)
+# new_password = "".join(new_password)
+# print(new_password)
